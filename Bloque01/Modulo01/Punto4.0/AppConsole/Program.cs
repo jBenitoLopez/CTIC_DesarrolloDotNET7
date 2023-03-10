a1b9ef941ad97e07a799a5977a17b5878811b5d0
jBenitoLopez <lopez.jbenito@gmail.com>
2023-03-10 18:59:10 +0100

    add AppConsole 4.0

---
+++ Bloque01/Modulo01/Punto4.0/AppConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace AppConsole;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            Console.WriteLine(string.Format("argument {0}: {1}", i + 1, args[i]));
+        }
+
+        Console.Write("Escribe algo: ");
+        string texto = Console.ReadLine() + "";
+        Console.WriteLine(texto.ToUpper());
+
+        Console.ReadLine();
+    }
+}

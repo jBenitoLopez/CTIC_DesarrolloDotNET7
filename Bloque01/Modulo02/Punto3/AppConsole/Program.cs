4b17120f86b2494ee76edfd00b07fa98c280a466
jBenitoLopez <lopez.jbenito@gmail.com>
2023-03-10 20:40:39 +0100

    add AppConsole m2p3

---
+++ Bloque01/Modulo02/Punto3/AppConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int num1 = 100;
+int num2 = 25;
+
+int suma = num1 + num2;
+
+Console.WriteLine(@$"La suma de 
+        {num1} + {num2} = {suma} 
+ ");

8628adc644972e51d87c5eb88611aeb6bbba7714
jBenitoLopez <lopez.jbenito@gmail.com>
2023-03-12 18:42:12 +0100

    add demo operadores ternarios m3p1

---
+++ Bloque01/Modulo03/Punto1/demoOperadorTernario/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace demoOperadorTernario;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Dame un numero: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        string resultado = numero >= 0 ? $"El Numero {numero} es positivo" : "Numero es negativo";
+        System.Console.WriteLine(resultado);
+
+    }
+}

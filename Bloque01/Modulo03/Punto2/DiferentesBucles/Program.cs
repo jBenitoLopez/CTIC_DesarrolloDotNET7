8e2ed72fd8a097b19e51776a1ef80e2723c923d8
jBenitoLopez <lopez.jbenito@gmail.com>
2023-03-12 21:23:35 +0100

    add: m3p2 Demo Diferentes bucles

---
+++ Bloque01/Modulo03/Punto2/DiferentesBucles/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace DiferentesBucles;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\n----- WHILE - Fibonacci ----");
+        int numerosTotalAMostrar = 10;
+
+        long num1 = 0;
+        long num2 = 1;
+
+        Console.WriteLine($"{num1}");
+        Console.WriteLine($"{num2}");
+
+        while (numerosTotalAMostrar-- > 0)
+        {
+            long sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+
+            Console.WriteLine($"{num2}");
+        }
+
+        Console.WriteLine("\n----- DO WHILE - Fibonacci ----");
+        num1 = 0;
+        num2 = 1;
+        numerosTotalAMostrar = 10;
+        do
+        {
+            long sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+
+            Console.WriteLine($"{num2}");
+        } while (--numerosTotalAMostrar > 0);
+
+
+        Console.WriteLine("\n----- FOR - Fibonacci ----");
+        num1 = 0;
+        num2 = 1;
+        numerosTotalAMostrar = 10;
+        
+        for (int i = numerosTotalAMostrar; i > 0; i--)
+        {
+            long sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+
+            Console.WriteLine($"{num2}");
+        }
+
+
+
+    }
+}

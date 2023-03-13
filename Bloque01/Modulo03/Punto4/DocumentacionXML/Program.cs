49634f9b82f1f8d3983f6ac725d987154c9f5162
jBenitoLopez <lopez.jbenito@gmail.com>
2023-03-13 07:53:03 +0100

    add m3p4 documentacion xml

---
+++ Bloque01/Modulo03/Punto4/DocumentacionXML/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace DocumentacionXML;
+
+/// <sumary>
+/// Esta es la clase principal
+/// anunciada en un comentario XML
+/// </sumary>
+class Program
+{
+    //
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}

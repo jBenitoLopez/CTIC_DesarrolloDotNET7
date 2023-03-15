67c6f3c5867304020edbf259a5a34f1850f001af
jBenitoLopez <lopez.jbenito@gmail.com>
2023-03-15 15:47:20 +0100

    add: Evaluation B1 M3

---
+++ Bloque01/Modulo03/_Evaluacion/Evaluacion_1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Evaluacion_1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        // for (int i = 0; i < 10; i++)
+        // {
+        //     if(i == 5){
+        //         return;
+        //     }
+        //     Console.WriteLine(i);
+        // }
+
+//         string[] nombres = { "Juan", "Pepe", "Ana", "Eva" };
+// string[] otros = nombres[1..];
+// nombres[0] = "Antonio";
+// Console.WriteLine(otros[^1]);
+
+
+
+    }
+}

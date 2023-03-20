30746465cee8eaca6ae881318ac200c56cdb76ef
jBenitoLopez <lopez.jbenito@gmail.com>
2023-03-20 20:09:44 +0100

    add: jugando con Enumerados

---
+++ Bloque02/Modulo05/Punto2/JugandoConEnumerados/Alumno.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JugandoConEnumerados;
+public class Alumno : Persona
+{
+    public string[] ListaAsignaturas { get; set; }
+    public string Aula { get; set; }
+
+    public override void MostrarInformacion()
+    {
+        Console.WriteLine("El nombre es " + Nombre + " " + Apellidos);
+    }
+}
---
+++ Bloque02/Modulo05/Punto2/JugandoConEnumerados/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace JugandoConEnumerados;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Persona persona = new Persona("Rubén", "Rubio", new DateTime(1950, 1, 1));
+        persona.TipoPersona = TipoPersona.Alumno;
+        persona.TipoPersonaBitByBit = TipoPersonaBitByBit.Alumno | TipoPersonaBitByBit.Vigilante;
+        if (persona.TipoPersonaBitByBit.HasFlag(TipoPersonaBitByBit.Alumno))
+        {
+            System.Console.WriteLine("Es un alumno!!!");
+        }
+        System.Console.WriteLine($"Tipo persona ({persona.TipoPersona}) es {(int)persona.TipoPersona}");
+        System.Console.WriteLine($"BitABit {persona.TipoPersonaBitByBit}");
+        persona.MostrarInformacion();
+    }
+}

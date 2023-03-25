016b637ebbad37da786a35e3190a0d5419a7ffbc
jBenitoLopez <lopez.jbenito@gmail.com>
2023-03-25 15:57:42 +0100

    add: stringbuilder

---
+++ Bloque02/Modulo05/Punto5/StringBuilder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilderApp;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a StringBuilder that expects to hold 50 characters.
+        // Initialize the StringBuilder with "ABC".
+        StringBuilder sb = new StringBuilder("ABC", 50);
+
+        // Append three characters (D, E, and F) to the end of the StringBuilder.
+        sb.Append(new char[] { 'D', 'E', 'F' });
+
+        // Append a format string to the end of the StringBuilder.
+        sb.AppendFormat("GHI{0}{1}", 'J', 'k');
+
+        // Display the number of characters in the StringBuilder and its string.
+        Console.WriteLine("{0} chars: {1}", sb.Length, sb.ToString());
+
+        // Insert a string at the beginning of the StringBuilder.
+        sb.Insert(0, "Alphabet: ");
+
+        // Replace all lowercase k's with uppercase K's.
+        sb.Replace('k', 'K');
+
+        // Display the number of characters in the StringBuilder and its string.
+        Console.WriteLine("{0} chars: {1}", sb.Length, sb.ToString());
+    }
+}

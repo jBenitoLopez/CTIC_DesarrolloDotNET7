1fa55281cd92bcf7f1a4d41a27a70fa794169ba9
jBenitoLopez <lopez.jbenito@gmail.com>
2023-03-30 18:17:33 +0200

    add: Registros

---
+++ Bloque02/Modulo06/Punto5/Registros/Persona.cs
@@ -0,0 +1,20 @@
+﻿namespace Registros;
+// Declaración mediante registros posicionales
+// public record Persona(string Nombre, string Apellidos);
+
+// constructores adicionale
+public record Persona(string Nombre, string Apellidos)
+{
+    public Persona(string Nombre) : this(Nombre, "") { }
+};
+
+// Sintáxis con C# 9 (sigue siendo válida)
+public record PersonaRecord(string Nombre, string Apellidos); // Registros por referencia
+
+// Sintáxis con C# 10
+public record class PersonaClass2(string Nombre, string Apellidos); // Para registros por referencia
+public record struct PersonaStruct2(string Nombre, string Apellidos); // Para registros por valor asimilables a estructuras
+readonly record struct PersonaStruct3(string Nombre, string Apellidos);
+
+record struct PersonaStruct(string Nombre, string Apellidos);
+record class PersonaClass(string Nombre, string Apellidos);
---
+++ Bloque02/Modulo06/Punto5/Registros/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Registros;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Registro - Record");
+        var persona1 = new Persona("Rubén", "Rubio");
+        var persona2 = persona1;
+        var persona3 = new Persona("Rubén", "Rubio");
+        var persona4 = new Persona("Rubén", "R.");
+
+        Console.WriteLine($"Referencia: persona1 = persona2 {ReferenceEquals(persona1, persona2)}");
+        Console.WriteLine($"Valor: persona1 = persona2 {persona1.Equals(persona2)}");
+        Console.WriteLine($"Referencia: persona1 = persona3 {ReferenceEquals(persona1, persona3)}");
+        Console.WriteLine($"Valor: persona1 = persona3 {persona1.Equals(persona3)}");
+        Console.WriteLine($"Referencia: persona1 = persona4 {ReferenceEquals(persona1, persona4)}");
+        Console.WriteLine($"Valor: persona1 = persona4 {persona1.Equals(persona4)}");
+
+
+        Console.WriteLine("=== toString ===");
+        var persona = new Persona("Rubén", "Rubio");
+        Console.WriteLine(persona);
+
+        Console.WriteLine("=== Instanciación mediante expresiones con with ===");
+        var persona11 = new Persona("Rubén", "Rubio");
+        var persona12 = persona11 with { Nombre = "Fernando" };
+
+        Console.WriteLine($"Persona 11: {persona11}");
+        Console.WriteLine($"Persona 12: {persona12}");
+
+
+        Console.WriteLine("=== Registros por valor (record struct, C# 10+) ===");
+
+        var objTestStruct = new PersonaStruct("Rubén", "Rubio");
+        var objTestClass = new PersonaClass("Rubén", "Rubio");
+
+        Console.WriteLine(objTestStruct);
+        Console.WriteLine(objTestClass);
+
+
+
+    }
+}

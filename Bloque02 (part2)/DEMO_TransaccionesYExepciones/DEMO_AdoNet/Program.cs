7316074a78905ceebd653f5370f53b49956d24ad
jBenitoLopez <lopez.jbenito@gmail.com>
2023-06-21 18:42:38 +0200

    add: 10 - DEMO: Usando transacciones y excepciones

--- Bloque02 (part2)/DEMO_TransaccionesYExepciones/DEMO_AdoNet/Program.cs
+++ Bloque02 (part2)/DEMO_TransaccionesYExepciones/DEMO_AdoNet/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Reflection.PortableExecutable;
 
 internal class Program
@@ -9,76 +10,107 @@ internal class Program
         {
             conexion.Open();
 
-            SqlCommand cmdSelect = new SqlCommand("SELECT COUNT(*) Total FROM Person.Person", conexion);
-            SqlCommand cmdCount = new SqlCommand("SELECT TOP 3 BusinessEntityID, FirstName, LastName FROM Person.Person", conexion);
-            SqlCommand cmdUpdate = new SqlCommand("UPDATE Person.Person Set FirstName = FirstName + '!' WHERE BusinessEntityID in (285, 293)", conexion);
 
-            //TODO: Recorrer los registros
 
-            Console.WriteLine($"++ Hay: {cmdSelect.ExecuteScalar()} registron en la tabla.\n");
-            Console.WriteLine("\n===========  ===========  ===========  ===========  =========== \n");
-
-            using (SqlDataReader reader = cmdCount.ExecuteReader())
+            using (var trans = conexion.BeginTransaction())
             {
-                if (reader.HasRows)
+                try
                 {
+                    SqlCommand cmdSelect = new SqlCommand("SELECT TOP 15 BusinessEntityID, FirstName, LastName FROM Person.Person ORDER BY BusinessEntityID", conexion);
+                    cmdSelect.Transaction = trans;
 
-                    int index = 0;
-
-                    while (reader.Read())
+                    using (SqlDataReader reader = cmdSelect.ExecuteReader())
                     {
-                        //Console.WriteLine($">> BusinessEntityID: {reader[0].ToString()}");
-                        //Console.WriteLine($">> FirstName: {reader[1]}");
-                        //Console.WriteLine($">> LastName: {reader[2]}");
-                        //Console.WriteLine("----------- --- ----------- ");
+                        if (reader.HasRows)
+                        {
+                            int index = 0;
 
-                        Console.WriteLine($"Registro: {index++}");
-                        Console.WriteLine($">> BusinessEntityID: {reader.GetInt32(0)}");
-                        Console.WriteLine($">> FirstName: {reader.GetString(1)}");
-                        Console.WriteLine($">> LastName: {reader.GetString(2)}");
-                        Console.WriteLine("----------- --- ----------- --- ----------- --- ----------- ");
+                            while (reader.Read())
+                            {
+                                Console.Write($"Registro: {index++}");
+                                Console.Write($"; BusinessEntityID: {reader.GetInt32(0)}");
+                                Console.Write($"; FirstName: {reader.GetString(1)}");
+                                Console.Write($"; LastName: {reader.GetString(2)}\n");
+                            }
+                        }
+
+                        reader.Close();
+                    }
+
+                    SqlCommand cmdUpdate1 = new SqlCommand("UPDATE Person.Person Set FirstName = FirstName + '1' WHERE BusinessEntityID = 1", conexion);
+                    cmdUpdate1.Transaction = trans;
+                    var filasAfectadas1 = cmdUpdate1.ExecuteNonQuery();
+                    trans.Save("punto1");
+
+                    var filasAfectadas2 = 0;
+                    var filasAfectadas3 = 0;
+                    try
+                    {
+                        SqlCommand cmdUpdate2 = new SqlCommand("UPDATE Person.Person Set FirstName = FirstName + '2' WHERE BusinessEntityID = 2", conexion);
+                        cmdUpdate2.Transaction = trans;
+                        filasAfectadas2 = cmdUpdate2.ExecuteNonQuery();
+
+                        SqlCommand cmdUpdate3 = new SqlCommand("UPDATE Person.Person Set FirstName33 = FirstName + '3' WHERE BusinessEntityID = 3", conexion);
+                        cmdUpdate3.Transaction = trans;
+                        filasAfectadas3 = cmdUpdate3.ExecuteNonQuery();
+                    }
+                    catch (Exception)
+                    {
+                        filasAfectadas2 = 0;
+                        filasAfectadas3 = 0;
+                        trans.Rollback("punto1");
+                    }
+     
+
+
+                    SqlCommand cmdUpdate4 = new SqlCommand("UPDATE Person.Person Set FirstName = FirstName + '4' WHERE BusinessEntityID = 4", conexion);
+                    cmdUpdate4.Transaction = trans;
+                    var filasAfectadas4 = cmdUpdate4.ExecuteNonQuery();
+
+                    Console.WriteLine($"Filas Afectadas (1): {filasAfectadas1}; (2): {filasAfectadas2}; (3): {filasAfectadas3}; (4): {filasAfectadas4}");
+                    Console.WriteLine("=========================================================================================");
+
+                    using (SqlDataReader reader = cmdSelect.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            int index = 0;
+
+                            while (reader.Read())
+                            {
+                                Console.Write($"Registro: {index++}");
+                                Console.Write($"; BusinessEntityID: {reader.GetInt32(0)}");
+                                Console.Write($"; FirstName: {reader.GetString(1)}");
+                                Console.Write($"; LastName: {reader.GetString(2)}\n");
+                            }
+                        }
 
                     }
+
                 }
-
-                reader.Close();
-            }
-
-            var filasAfectadas = cmdUpdate.ExecuteNonQuery();
-            Console.WriteLine($"Registros modificados: {filasAfectadas}");
-
-            Console.WriteLine("\n===========  ===========  ===========  ===========  ===========\n");
-
-            using (SqlDataReader reader = cmdCount.ExecuteReader())
-            {
-                if (reader.HasRows)
+                catch (IndexOutOfRangeException ex)
                 {
-
-                    int index = 0;
-
-                    while (reader.Read())
-                    {
-                        //Console.WriteLine($">> BusinessEntityID: {reader[0].ToString()}");
-                        //Console.WriteLine($">> FirstName: {reader[1]}");
-                        //Console.WriteLine($">> LastName: {reader[2]}");
-                        //Console.WriteLine("----------- --- ----------- ");
-
-                        Console.WriteLine($"Registro: {index++}");
-                        Console.WriteLine($">> BusinessEntityID: {reader.GetInt32(0)}");
-                        Console.WriteLine($">> FirstName: {reader.GetString(1)}");
-                        Console.WriteLine($">> LastName: {reader.GetString(2)}");
-                        Console.WriteLine("----------- --- ----------- --- ----------- --- ----------- ");
-
-                    }
+                    Console.WriteLine($"\nERROR OutOfRangeException: {ex.Message}");
+                    trans.Rollback();
+                    throw;
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine($"\nERROR SqlException: {ex.Message}");
+                    trans.Rollback();
+                    throw;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"\nERROR Exception: {ex.Message}");
+                    trans.Rollback();
+                    throw;
                 }
 
-                reader.Close();
+                trans.Commit();
             }
-
-  
 
             conexion.Close();
-
         }
 
          Console.WriteLine("----------- END ----------- ");
